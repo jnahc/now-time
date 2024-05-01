763e22a476796b0f663585ab1c4ef18d03414f6a
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2024-05-01 13:09:52 -0700

    fixed backticks in index

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,6 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -30,9 +21,9 @@ export default function Home({ allPostsData }) {
       <section className={'${utilStyles.headingMd} ${utilStyles.padding1px}'}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title}) =>(
+          {allPostsData.map(({ id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={'/posts/${id'}>{title}</Link>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
@@ -43,4 +34,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
