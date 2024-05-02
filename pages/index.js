bd73c452145fb2174b700917dac9311abd1257b4
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2024-05-02 11:11:10 -0700

    removed more unused code, imported clock library

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Date from '../components/date';
+import Clock from 'react-live-clock';
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -11,16 +11,10 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>What's the time?</p>
+        <p>
+          <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+        </p>
       </section>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
